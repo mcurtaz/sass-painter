85e3457961015c925fcf201475c8052783d92eb8
Michele Curtaz <mcurtaz@gmail.com>
2020-08-28 23:09:00 +0200

    Jumbo work in progress

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,7 @@ mix
     .sass('src/app.scss', 'dist/')
     .copy('src/index.html', 'dist/')
     .copyDirectory('src/imgs', 'dist/imgs')
+    .options({ processCssUrls: false })
 ;
 
 // Full API
